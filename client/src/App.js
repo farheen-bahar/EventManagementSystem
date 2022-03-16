a49c0a4e2ee8f94a3bd5593d722ccc2a1900b381
Anil <anjanil@deloitte.com>
2022-03-16 12:39:57 +0530

    Login API

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import {Routes, Route,  Navigate} from 'react-router-dom'
 
 import Header from './components/Header/Header';
 import Form from './components/Form/Form';
+import Login from './components/Form/Login';
 import './styles.css'
 import Home from './components/Home/Home';
 
@@ -11,6 +12,7 @@ function App() {
       <Header/>
       <Routes>
         <Route path='/' element={<Form/>}/>
+        {/* <Route path='/api/login' element={<Login/>}/> */}
         <Route path='/home' element={<Home/>}/>
         <Route path='*' element={<Navigate replace to="/home"/>}/>
       </Routes>
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const otproutes =  require('./routes/OTP')
+const userloginroutes = require('./routes/User')
 
 const app = express()
 app.use(express.json())
@@ -10,15 +11,12 @@ app.use(bodyParser.json({extended:true}))
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(cors())
 
-app.use('/api',otproutes)
 
+// begin mongoose setup
 
-app.get('/', function (req, res) {
-    console.log('route / is accessed.');
-    res.send('Hi');
-  });
+//const CONNECTION_URL = 'mongodb+srv://feedbacktracker:feedbacktracker123@mern.if1a7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const CONNECTION_URL = "mongodb+srv://event_management:event_management@cluster0.tcr4k.mongodb.net/Cluster0?retryWrites=true&w=majority";
 
-const CONNECTION_URL = 'mongodb+srv://feedbacktracker:feedbacktracker123@mern.if1a7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser:true, useUnifiedTopology:true})
@@ -28,3 +26,23 @@ mongoose.connect(CONNECTION_URL, {useNewUrlParser:true, useUnifiedTopology:true}
 .catch(err=>
     console.log(err)
 )
+
+const db = mongoose.connection
+db.once('open', _=>{
+    console.log("Datatbase Connection Complete", CONNECTION_URL)
+})
+
+db.on("error", err=>{
+    console.log("Connection error", err)
+})
+// end setup
+
+app.use('/api',otproutes);
+app.use('/user',userloginroutes)
+
+
+app.get('/', function (req, res) {
+    console.log('route / is accessed.');
+    res.send('Hi');
+  });
+
